37401390b28b266786afc60974d3731332767f01
Nuwan Madushanka Jaliyagoda <NuwanJ@users.noreply.github.com>
2017-05-18 18:46:38 +0530

    Add files via upload

---
+++ arduino/arduino.ino
@@ -0,0 +1,16 @@
+
+int pwmPin = 0;
+int analogPin = A1;
+
+void setup() {
+
+  pinMode(pwmPin, OUTPUT);
+  pinMode(analogPin, INPUT);
+}
+
+void loop() {
+  
+  int val = analogRead(analogPin); // Read analog voltage on pin 2 (A1)
+  analogWrite(pwmPin, val / 4);
+  
+}
